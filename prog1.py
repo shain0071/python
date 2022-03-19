e78fd31540be7d39a4c6e1858c3470b057bf6023
shain0071 <101937005+shain0071@users.noreply.github.com>
2022-03-19 23:31:21 +0800

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,9 @@
+def adding(x , y):
+    return x + y
+ 
+a = int(input("Enter first number :" ))
+b = int(input("Enter second number :"))
+ 
+sum = adding(a , b)
+ 
+print("addition of {} and {} is {}".format(a,b,sum))
